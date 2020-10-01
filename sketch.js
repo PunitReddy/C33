05f7e96136b7b910d0cc0c3fc3caedf7a39ea9a7
PunitReddy <punitvaibhavaramreddy@glendaleacademy.net>
2020-10-01 22:08:43 +0530

    debugging

--- sketch.js
+++ sketch.js
@@ -93,8 +93,11 @@ function mouseReleased(){
 }
 
 function keyPressed(){
-    if(keyCode === 32){
+    if(keyCode === 32 && bird.body.speed < 1){
+        Matter.Body.setPosition(bird.body, {x: 200, y: 20})
+        Matter.Body.setAngle(bird.body, 0);
        slingshot.attach(bird.body);
+       bird.trajectory = []
     }
 }
 
